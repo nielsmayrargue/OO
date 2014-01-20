8e302efa27bee3a2d445928613b3be3b80835ab2
jubeo <jubeo@jubeo-K43SJ.(none)>
2014-01-20 14:51:37 +0100

    finalisation OO06

--- 06-Instance-vs-Class/restaurant.rb
+++ 06-Instance-vs-Class/restaurant.rb
@@ -1,12 +1,15 @@
 class Restaurant
   @number_of_ratings = 0
-  attr_reader :avg_rating
+  attr_reader :avg_rating, :name
+class << self
+  attr_accessor :number_of_ratings
+end
 
-  def self.get_nmb_ratings
+  def self.number_of_ratings2 #convention veut que le getter s'appelle comme la variable cherchée
     @number_of_ratings
   end
 
-  def self.write_nmb_ratings
+  def self.increment_ratings
     @number_of_ratings += 1
   end
 
@@ -16,14 +19,11 @@ class Restaurant
   	@food_type = food_type
     @ratings = [rating]
     @avg_rating = rating
+    Restaurant.number_of_ratings += 1
   end
 
-  #def number_of_rating_reader
-  #	@@number_of_ratings
-  #end
-
   def rate(rating)
-    Restaurant.write_nmb_ratings
+    Restaurant.increment_ratings
     @ratings << rating
     sum_of_rating = 0
     @avg_rating = @ratings.inject{|sum_of_rating,x| sum_of_rating + x }.to_f / @ratings.length
@@ -32,16 +32,17 @@ class Restaurant
 end
 
 #tests
-Quick = Restaurant.new("Quick", "16 rue de la Malbouffe", 0, "junkfood")
-Kebab = Restaurant.new("Kebab", "Mars", 10, "SublimFood")
-puts Restaurant.get_nmb_ratings
-puts Quick.avg_rating
-puts Kebab.avg_rating
-Quick.rate(9)
-Kebab.rate(1)
-Quick.rate(9)
-Kebab.rate(0)
-puts Restaurant.get_nmb_ratings
-puts Quick.avg_rating
-puts Kebab.avg_rating
+quick = Restaurant.new("Quick", "16 rue de la Malbouffe", 0, "junkfood")
+kebab = Restaurant.new("Kebab", "Mars", 10, "SublimFood")
+puts "#{Restaurant.number_of_ratings2} is the number of ratings using a method-accessor"
+puts "#{quick.avg_rating} is the rating of the restaurant #{quick.name}"
+puts "#{kebab.avg_rating} is the rating of the restaurant #{kebab.name}"
+puts "a rating of #{quick.rate(9)} has beed added to the #{quick.name} Restaurant"
+puts "a rating of #{kebab.rate(1)} has beed added to the #{kebab.name} Restaurant"
+puts "a rating of #{quick.rate(8)} has beed added to the #{quick.name} Restaurant"
+puts "a rating of #{kebab.rate(0)} has beed added to the #{kebab.name} Restaurant"
+puts "#{Restaurant.number_of_ratings2} is the number of ratings using a method-accessor"
+puts "The average rating of #{quick.name} is #{quick.avg_rating}"
+puts "The average rating of #{kebab.name} is #{kebab.avg_rating}"
+puts "#{Restaurant.number_of_ratings} is the number of ratings using a class accessor"
 
